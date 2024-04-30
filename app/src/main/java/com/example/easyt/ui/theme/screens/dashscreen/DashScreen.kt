f012b5e120a0ce65a1fae2d55e6746a6d21a645b
corel-dev <tony.kyalo06@gmail.com>
2024-04-30 18:59:01 +0300

    Third commit

--- app/src/main/java/com/example/easyt/ui/theme/screens/dashscreen/DashScreen.kt
+++ app/src/main/java/com/example/easyt/ui/theme/screens/dashscreen/DashScreen.kt
@@ -1,5 +1,6 @@
 package com.example.easyt.ui.theme.screens.dashscreen
 
+import android.icu.text.CaseMap.Title
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,12 +15,18 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.BottomAppBarDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
@@ -36,6 +43,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -49,92 +57,267 @@ import com.example.easyt.ui.theme.EasyTTheme
 @Composable
 fun DashScreen(navController: NavHostController) {
     Column(modifier = Modifier
-        .fillMaxSize()){
+        .fillMaxSize()) {
         //TopAppBar
-        TopAppBar(title = { Text(text = "Cities", color = Color.Black) },
+        TopAppBar(title = {
+            Text(
+                text = "EasyT",
+                color = Color.Black,
+                fontFamily = FontFamily.Cursive
+            )
+        },
             colors = TopAppBarDefaults.mediumTopAppBarColors(Color.White),
             navigationIcon = {
                 IconButton(onClick = { }) {
-                    Icon(imageVector = Icons.Default.Menu, contentDescription = "arrowback",
-                        tint = Color.Black)
+                    Icon(
+                        imageVector = Icons.Default.Menu, contentDescription = "",
+                        tint = Color.Black
+                    )
 
                 }
             },
             actions = {
                 IconButton(onClick = { /*TODO*/ }) {
-                    Icon(imageVector = Icons.Default.Notifications, contentDescription = "share",
-                        tint = Color.Black)
+                    Icon(
+                        imageVector = Icons.Default.Notifications, contentDescription = "share",
+                        tint = Color.Black
+                    )
                 }
                 IconButton(onClick = { /*TODO*/ }) {
-                    Icon(imageVector = Icons.Default.Search, contentDescription = "add",
-                        tint = Color.Black)
-                }
-                IconButton(onClick = { /*TODO*/ }) {
-                    Icon(imageVector = Icons.Default.Share, contentDescription = "share",
-                        tint = Color.Black)
+                    Icon(
+                        imageVector = Icons.Default.AccountCircle, contentDescription = "add",
+                        tint = Color.Black
+                    )
                 }
             })
-        //end of TopAppBar
-        var search by remember {
-            mutableStateOf("")
-        }
-        Text(text = "Explore the",
+        Text(
+            text = "Explore the",
             fontSize = 35.sp,
             modifier = Modifier
-                .padding(start = 20.dp, top = 20.dp))
-        Text(text = "Beautiful World",
+                .padding(start = 20.dp, top = 20.dp)
+        )
+        Text(
+            text = "Beautiful World",
             fontSize = 35.sp,
             fontWeight = FontWeight.ExtraBold,
             modifier = Modifier
-                .padding(start = 20.dp, top = 3.dp))
+                .padding(start = 20.dp, top = 3.dp)
+        )
+        Spacer(modifier = Modifier.width(100.dp))
+        var search by remember {
+            mutableStateOf("")
+        }
         Spacer(modifier = Modifier.height(5.dp))
-        Column(modifier = Modifier
-            .verticalScroll(rememberScrollState())) {
-            Row (modifier = Modifier
-                .padding(25.dp)){
+        //Column 1
+        Column(
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+        ) {
+            Row(
+                modifier = Modifier
+                    .padding(25.dp)
+            ) {
                 Column {
-                    Card(modifier = Modifier
-                        .height(90.dp)
+                    Card(onClick = { /*TODO*/ },modifier = Modifier
+                        .height(85.dp)
                         .width(150.dp)) {
-                        Box {
-                            Image(painter = painterResource(id = R.drawable.img_3), contentDescription = "Tokyo",
-                                modifier = Modifier.padding(start = 10.dp, top = 15.dp)
-                                    .clip(shape = CircleShape))
-
-                            Column {
-                                androidx.compose.material3.Text(
-                                    text = "Hotel",
-                                    fontWeight = FontWeight.Bold,
-                                    fontSize = 20.sp,
+                            Box {
+                                Image(
+                                    painter = painterResource(id = R.drawable.img_3),
+                                    contentDescription = "Tokyo",
                                     modifier = Modifier
-                                        .padding(start = 80.dp, top = 25.dp))}
+                                        .padding(start = 10.dp, top = 15.dp)
+                                        .clip(shape = CircleShape)
+                                )
 
+                                Column {
+                                    androidx.compose.material3.Text(
+                                        text = "Hotel",
+                                        fontWeight = FontWeight.Bold,
+                                        fontSize = 20.sp,
+                                        modifier = Modifier
+                                            .padding(start = 80.dp, top = 25.dp)
+                                    )
+                                }
+
+                            }
                         }
-                    }
                 }
                 Spacer(modifier = Modifier.width(20.dp))
                 Column {
-                    Card(modifier = Modifier
-                        .height(90.dp)
+                    Card(onClick = { /*TODO*/ },modifier = Modifier
+                        .height(85.dp)
                         .width(155.dp)) {
-                        Box {
-                            Image(painter = painterResource(id = R.drawable.img_6,),contentDescription = "Tokyo",
-                                modifier = Modifier.padding(start = 10.dp, top = 15.dp)
-                                    .clip(shape = CircleShape))
-
-                            Column {
-                                androidx.compose.material3.Text(
-                                    text = "Holiday",
-                                    fontWeight = FontWeight.Bold,
-                                    fontSize = 20.sp,
+                            Box {
+                                Image(
+                                    painter = painterResource(id = R.drawable.img_6,),
+                                    contentDescription = "Tokyo",
                                     modifier = Modifier
-                                        .padding(start = 80.dp, top = 25.dp))}
+                                        .padding(start = 10.dp, top = 15.dp)
+                                        .clip(shape = CircleShape)
+                                )
 
-                        }
+                                Column {
+                                    androidx.compose.material3.Text(
+                                        text = "Holiday",
+                                        fontWeight = FontWeight.Bold,
+                                        fontSize = 20.sp,
+                                        modifier = Modifier
+                                            .padding(start = 80.dp, top = 25.dp)
+                                    )
+                                }
+
+                            }
                     }
                 }
             }
         }
+        //End of column 1
+        //column 2
+        Column(
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+        ) {
+            Row(
+                modifier = Modifier
+                    .padding(25.dp)
+            ) {
+                Column {
+                   Card(onClick = { /*TODO*/ },modifier = Modifier
+                       .height(85.dp)
+                       .width(150.dp)) {
+
+                           Box {
+                               Image(
+                                   painter = painterResource(id = R.drawable.img_9),
+                                   contentDescription = "Tokyo",
+                                   modifier = Modifier
+                                       .padding(start = 10.dp, top = 20.dp)
+                                       .clip(shape = CircleShape)
+                               )
+
+                               Column {
+                                   androidx.compose.material3.Text(
+                                       text = "Taxi",
+                                       fontWeight = FontWeight.Bold,
+                                       fontSize = 20.sp,
+                                       modifier = Modifier
+                                           .padding(start = 105.dp, top = 30.dp)
+                                   )
+                               }
+
+                           }
+                   }
+                }
+                Spacer(modifier = Modifier.width(20.dp))
+                Column {
+                    Card(onClick = { /*TODO*/ },modifier = Modifier
+                        .height(85.dp)
+                        .width(155.dp)) {
+
+                            Box {
+                                Image(
+                                    painter = painterResource(id = R.drawable.img_8,),
+                                    contentDescription = "Tokyo",
+                                    modifier = Modifier
+                                        .padding(start = 10.dp, top = 20.dp)
+                                        .clip(shape = CircleShape)
+                                )
+
+                                Column {
+                                    androidx.compose.material3.Text(
+                                        text = "Tickets",
+                                        fontWeight = FontWeight.Bold,
+                                        fontSize = 20.sp,
+                                        modifier = Modifier
+                                            .padding(start = 80.dp, top = 30.dp)
+                                    )
+                                }
+
+                            }
+                    }
+                }
+            }
+        }
+        //End of column 2
+        //column 3
+        Column(
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+        ) {
+            Row(
+                modifier = Modifier
+                    .padding(25.dp)
+            ) {
+                Column {
+                   Card(onClick = { /*TODO*/ }, modifier = Modifier
+                       .height(85.dp)
+                       .width(150.dp)) {
+
+                           Box {
+                               Image(
+                                   painter = painterResource(id = R.drawable.img_10),
+                                   contentDescription = "Tokyo",
+                                   modifier = Modifier
+                                       .padding(start = 10.dp, top = 15.dp)
+                                       .clip(shape = CircleShape)
+                               )
+
+                               Column {
+                                   androidx.compose.material3.Text(
+                                       text = "Plane",
+                                       fontWeight = FontWeight.Bold,
+                                       fontSize = 20.sp,
+                                       modifier = Modifier
+                                           .padding(start = 88.dp, top = 25.dp)
+                                   )
+                               }
+
+                           }
+                   }
+                }
+                Spacer(modifier = Modifier.width(20.dp))
+                Column {
+                    Card(onClick = { /*TODO*/ }, modifier = Modifier
+                        .height(85.dp)
+                        .width(155.dp)) {
+
+                            Box {
+                                Image(
+                                    painter = painterResource(id = R.drawable.img_11,),
+                                    contentDescription = "Tokyo",
+                                    modifier = Modifier
+                                        .padding(start = 10.dp, top = 15.dp)
+                                        .clip(shape = CircleShape)
+                                )
+
+                                Column {
+                                    androidx.compose.material3.Text(
+                                        text = "Harbour",
+                                        fontWeight = FontWeight.Bold,
+                                        fontSize = 19.sp,
+                                        modifier = Modifier
+                                            .padding(start = 80.dp, top = 25.dp)
+                                    )
+                                }
+
+                            }
+                    }
+                }
+                //end of TopAppBar
+            }
+        }
+        //End of column 1
+        //End of searchbar
+        Spacer(modifier = Modifier.height(120.dp))
+        BottomAppBar(contentColor = Color.Blue, actions = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Default.Notifications, contentDescription = "share",
+                        tint = Color.Black
+                    )
+                }
+            })
     }
 }
 @Composable
